45b900b5a889a34462caea8c67b10ec3171c1ea5
Ian Nelson <130817783+eniscool1@users.noreply.github.com>
2023-09-30 17:04:20 -0500

    backtrack

--- api/index.py
+++ api/index.py
@@ -1,19 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask
 app = Flask(__name__)
 
 @app.route("/api/python")
 def hello_world():
     return "<p>Hello, World!</p>"
 
-@app.route("/")
-def main():
-    return render_template("home.html")
 
-
-@app.route("/", methods = ["POST"])
-def FFACalc():
-
-    return render_template("Resultpage.html", result = result)
 
 
 if __name__ == "__main__":
