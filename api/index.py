d8a37d0a6ca287d78a50eb0fa750d09641de0d02
Ian Nelson <130817783+eniscool1@users.noreply.github.com>
2023-09-30 16:42:24 -0500

    template

--- api/index.py
+++ api/index.py
@@ -4,3 +4,17 @@ app = Flask(__name__)
 @app.route("/api/python")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/")
+def main():
+    return render_template("FFApage.html")
+
+
+@app.route("/", methods = ["POST"])
+def FFACalc():
+
+    return render_template("Resultpage.html", result = result)
+
+
+if __name__ == "__main__":
+    app.run()
